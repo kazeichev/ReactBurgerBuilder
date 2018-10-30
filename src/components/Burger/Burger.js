538953f1d8d33d75d925262313ac0396c8ce17cf
Nikita Kazeichev <kazeichev@yandex.ru>
2018-10-30 11:49:41 +0300

    Добавил панель управления компонентами бургера

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,11 +4,24 @@ import classes from './Burger.module.css';
 import BurgerIngredience from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+    let transformedIngredients = Object.keys(props.ingredients)
+        .map(key => {
+            return [...Array(props.ingredients[key])]
+            .map( (_, i) => {
+                return <BurgerIngredience key={key + i}  type={key}/>;
+            })
+        }).reduce((array, el) => {
+            return array.concat(el);
+        }, []);
+
+    if  (transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please, start adding ingredients!</p>;
+    }
+    
     return (
         <div className={classes.Burger}>
             <BurgerIngredience type="bread-top" />
-            <BurgerIngredience type="cheese" />
-            <BurgerIngredience type="meat" />
+            {transformedIngredients}
             <BurgerIngredience type="bread-bottom" />
         </div>
     );
